4706a47b2a8d2748414572716bed336eef0bbd82
Dmytro Morozov <morosovdmitry@gmail.com>
2017-01-23 15:42:30 +0200

    Adding error case for jsonp

--- index-jsonp.php
+++ index-jsonp.php
@@ -1,13 +1,24 @@
 <?php
 $callback = $_GET['callback'];
-$url = urldecode($_GET['url']);
-header("Content-Type: application/json");
-$response = [
-    'success' => 1,
-    'data' => [
-        'url' => urlencode($url)
-    ]
-];
+if(isset($_GET['url'])){
+    $url = urldecode($_GET['url']);
+    header("Content-Type: application/json");
+    $response = [
+        'success' => 1,
+        'data' => [
+            'url' => urlencode($url)
+        ]
+    ];
+}
+else {
+    $response = [
+        'success' => 0,
+        'data' => [
+            'url' => 'Image is missing'
+        ]
+    ];
+}
+
 echo $_GET['callback'] . '( ' . json_encode($response) . ' )';
 exit();
 ?>
